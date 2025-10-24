e668cef6fdb3664c52c46dcb17e07e045fce8001
kxzazz <aman642.g@gmail.com>
2025-10-24 05:45:37 +0530

    ⚙️: Setup db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { db } from "@/db/db";
+import { postsTable, usersTable } from "@/db/schema";
+const Page = async () => {
+  const users = await db.select().from(usersTable);
+  const posts = await db.select().from(postsTable);
 
-const Page = () => {
   return (
     <div className="">
-      <Button> hello </Button>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </div>
   );
 };
